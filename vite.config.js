2d273b995a219cffba7c9aa84edee73dedd9a180
Sathwika-n <snemurugommula@clarku.edu>
2024-11-26 23:43:22 +0530

    Google signin

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/", // For root directory deployment
+  define: {
+    "process.env": process.env, // Optional, depending on your setup
+  },
 });
